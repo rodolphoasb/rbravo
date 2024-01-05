dd77d54b5dda9bbd9a2a1c9e91f51d43366140e0
Rodolpho Bravo <bravorodolpho@gmail.com>
2024-01-05 15:46:39 -0300

    chore: add mdx support

---
+++ app/routes/posts+/_index.tsx
@@ -0,0 +1,7 @@
+export default function PostsPages() {
+  return (
+    <div>
+      <h1>Posts</h1>
+    </div>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
+import mdx from '@mdx-js/rollup'
 import { unstable_vitePlugin as remix } from '@remix-run/dev'
 import morgan from 'morgan'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 import { remixDevTools } from 'remix-development-tools/vite'
 import { defineConfig, type ViteDevServer } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -11,6 +14,9 @@ export default defineConfig({
     remix(remixConfig),
     remixDevTools(),
     tsconfigPaths(),
+    mdx({
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    }),
   ],
 })
 
